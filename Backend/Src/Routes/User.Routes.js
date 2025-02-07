2430f6ec7bb7550a96b68ec29df347a332631e07
Rishu Raj <rishuraj1127@gmail.com>
2025-02-07 11:33:01 +0530

    User Routers

--- Backend/Src/Routes/User.Routes.js
+++ Backend/Src/Routes/User.Routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-
+import { registerUser, loginUser } from "../Controller/User.controller.js";
 const router = Router();
 
-
+router.route("/registerUser").post(registerUser);
+router.route("/loginUser").post( loginUser);
 
 export default router;
