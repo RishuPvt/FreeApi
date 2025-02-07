78443dbe4afd097b1eca4359ca618a81462a6943
Rishu Raj <rishuraj1127@gmail.com>
2025-02-07 11:32:35 +0530

    User : Verify Jwt Token

---
+++ Backend/Src/Middleware/Auth.middleware.js
@@ -0,0 +1,32 @@
+import ApiError from "../Utility/ApiError.js";
+import jwt from "jsonwebtoken";
+import prisma from "../DB/Database.js";
+
+export const verifyJWT = async (req, res) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decodedToken?.id,
+      },
+    });
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+};
