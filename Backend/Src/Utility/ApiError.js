9c9ff3c9fefc43ba98193d7546e31b5f48acf1b6
Rishu Raj <rishuraj1127@gmail.com>
2025-02-11 12:43:11 +0530

    First commit

--- Backend/Src/Utility/ApiError.js
+++ Backend/Src/Utility/ApiError.js
@@ -1,8 +1,7 @@
 class ApiError extends Error{
-    constructor(statusCode , message = "Something went wrong", errors = []){
+    constructor(statusCode , message = "Something went wrong", errors ){
         super(message);
         this.statusCode = statusCode;
-        this.data = null;
         this.message = message; 
         this.success = false;
         this.errors = errors;
--- Backend/prisma/migrations/20250211055716_init/migration.sql
+++ Backend/prisma/migrations/20250211055716_init/migration.sql
@@ -19,20 +19,12 @@ CREATE TABLE "Project" (
     "userId" TEXT NOT NULL,
     "language" TEXT NOT NULL,
     "framework" TEXT NOT NULL,
+    "fileUrl" TEXT[],
+    "downloadCount" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Download" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "ProjectId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Download_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -40,9 +32,3 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Download" ADD CONSTRAINT "Download_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Download" ADD CONSTRAINT "Download_ProjectId_fkey" FOREIGN KEY ("ProjectId") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
