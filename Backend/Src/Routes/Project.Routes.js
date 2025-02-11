999af987df5a8edc1d348218f67624573b4a4955
Rishu Raj <rishuraj1127@gmail.com>
2025-02-11 12:42:43 +0530

    Status : Final project route

--- Backend/Src/Routes/Project.Routes.js
+++ Backend/Src/Routes/Project.Routes.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../Middleware/Auth.middleware.js";
-import { uploadProject, allProject, updateProject ,projectDetails } from "../Controller/Project.controller.js";
-
+import { uploadProject, allProject, updateProject ,projectDetails ,deleteProject, downloadProject , getDownloadCount} from "../Controller/Project.controller.js";
+import {upload} from "../Middleware/multer.middleware.js"
 const router = Router();
 
-router.route("/uploadproject").post(verifyJWT, uploadProject);
+router.route("/uploadproject").post(verifyJWT, upload.array("fileUrl", 50), uploadProject);
 router.route("/allproject").get(allProject);
 router.route("/updateProject/:projectId").patch(verifyJWT , updateProject);
 router.route("/projectDetails/:projectId").get( projectDetails);
+router.route("/deleteProject/:projectId").delete( verifyJWT , deleteProject);
+
+router.route("/downloadProject/:projectId").get(downloadProject)
+router.route("/downloadCount/:projectId").get(getDownloadCount)
 
 
 
