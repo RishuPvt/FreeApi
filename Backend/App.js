6899de21a8574396c7fd42a2439fa819d2108e8a
Rishu Raj <rishuraj1127@gmail.com>
2025-02-12 12:52:27 +0530

    Fixed issue

--- Backend/App.js
+++ Backend/App.js
@@ -1,7 +1,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 const app = express();
+
+const corsOptions = {
+  origin: " http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Origin", "Content-Type", "Accept", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
 
@@ -11,14 +20,12 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
-
 import UserRouter from "../Backend/Src/Routes/User.Routes.js";
 
 app.use("/api/v1/users", UserRouter);
 
+import ProjectRouter from "../Backend/Src/Routes/Project.Routes.js";
 
-import ProjectRouter from "../Backend/Src/Routes/Project.Routes.js"
-
-app.use("/api/v1/projects", ProjectRouter)
+app.use("/api/v1/projects", ProjectRouter);
 
 export { app };
