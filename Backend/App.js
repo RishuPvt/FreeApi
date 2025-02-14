f20505939947ac60ffcd5db7aeb8a3329363f3df
Rishu Raj <rishuraj1127@gmail.com>
2025-02-14 15:41:16 +0530

    Incress payload

--- Backend/App.js
+++ Backend/App.js
@@ -12,9 +12,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(express.static("public"));
 
