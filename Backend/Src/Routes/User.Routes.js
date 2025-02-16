43d3ee2cff405bc97b4e921d9d0c2ddf32ad1d30
Rishu Raj <rishuraj1127@gmail.com>
2025-02-16 12:22:12 +0530

    Final route

--- Backend/Src/Routes/User.Routes.js
+++ Backend/Src/Routes/User.Routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { registerUser, loginUser , logoutUser} from "../Controller/User.controller.js";
+import { registerUser, loginUser , logoutUser , currentUser} from "../Controller/User.controller.js";
 import { verifyJWT } from "../Middleware/Auth.middleware.js";
 const router = Router();
 
 router.route("/registerUser").post(registerUser);
 router.route("/loginUser").post( loginUser);
 router.route("/logoutUser").post( verifyJWT , logoutUser);
+router.route("/currentUser").get( verifyJWT , currentUser);
+
 
 
 export default router;
