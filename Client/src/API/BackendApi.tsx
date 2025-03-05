3f35738936967606b990562e47284bd9fbfdad38
Rishu Raj <rishuraj1127@gmail.com>
2025-03-05 23:32:34 +0530

    Fixed URl

--- Client/src/API/BackendApi.tsx
+++ Client/src/API/BackendApi.tsx
@@ -1,2 +1,2 @@
 // export const backendUrl = "http://localhost:8000/api/v1"
-export const backendUrl ="https://freeapi-v89s.onrender.com"
+export const backendUrl ="https://freeapi-v89s.onrender.com/api/v1"
